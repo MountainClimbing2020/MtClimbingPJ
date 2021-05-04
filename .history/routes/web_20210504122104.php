a378541c8d6a02c0eb323ff3343f4de01e21d29c
1984aomame <kafka.on.the.shoree@gmail.com>
2021-05-04 15:34:47 +0900

    　　　

---
+++ .history/routes/web_20210504122104.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// トップページ
+Route::get('/', function () {
+    return view('top');
+})->name('top');
+// ログイン機能
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+// このサイトについて
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+// あなたの思い出の景色
+Route::get('/memory/board', function () {
+    return view('memory/board');
+})->name('memory/board');
+Route::get('/memory/submit', function () {
+    return view('memory/submit');
+})->name('memory/submit');
+/*
+Route::post('/memory/confirm', 'ImageController@upload')->name('memory/confirm');
+*/
+Route::post('/memory/complete', 'ImageController@upload')->name('memory/complete');
